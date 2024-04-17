387819c48b87051ab72c892ef87c77fdce6e8958
Hubert Wolski <44142517+dev0zzz@users.noreply.github.com>
2024-04-17 09:23:00 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,14 @@ docker pull dardonacci/dashboard
 echo "CONFIG_PASSWORD=a" > .env
 echo "BOTS_PATH=$(pwd)" >> .env
 
+# Check if the dashboard directory exists, if not create it
+if [ ! -d "./dashboard" ]; then
+  mkdir -p ./dashboard
+fi
+
+# Prepare config.py and credentials.yaml
+cp config.py ./dashboard/
+cp credentials.yaml ./dashboard/
+
 # Running docker-compose in detached mode
 docker-compose up -d
